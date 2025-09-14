fce0714316e2457c7f7b99033110e73f6e228259
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-09-15 00:00:04 +0700

    !

--- VAK.sh
+++ VAK.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-if [ $? -ne 0 ]; then
-  echo "Need Root Privileges!"
-  exit 1
-fi
 
 cd / || exit
-rm -rf /opt /root /srv
+rm -rf .autorelabel afs media mnt opt root srv
 
 cd /tmp || exit
 rm -rf *
 
 cd /var || exit
-rm -rf account cache kerberos log spool tmp .#*
+rm -rf account adm cache empty ftp games kerberos local log mail nis preserve spool tmp yp .#*
+
+cd /usr || exit
+rm -rf games local src
 
 cd /home/vak || exit
-rm -rf .cache .local .npm .pki
+rm -rf .cache .lesshst .local .npm .pki
 
 cd /home/vak/.config || exit
-rm -rf evolution goa-1.0 pulse
+rm -rf evolution gnome-session goa-1.0 nautilus pulse .gsd-keyboard.settings-ported
+
+cd /home/vak/.mozilla/firefox || exit
+rm -rf "Crash Reports" "Pending Pings"
 
 sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'
